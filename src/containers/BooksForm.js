f0deff557eba53126ce7274da877af6b54de5c91
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-09 15:24:31 +0100

    Add handleChange to booksForm

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CATEGORIES } from '../constants/index';
 
-const BooksForm = () => (
-  <form>
-    <input type="text" />
-    <select>
-      {CATEGORIES.map(category => (
-        <option key={`${category}`}>{category}</option>
-      ))}
-    </select>
-    <input type="submit" />
-  </form>
-);
+const BooksForm = () => {
+  const { title, setTitle } = useState('');
+  const { category, setCategory } = useState('');
+
+  const handleChange = (target) => {
+    if (target.tagName === 'INPUT') {
+      setTitle(target.value);
+    } else if (target.tagName === 'SELECT') {
+      const text = target.options[target.selectedIndex].text;
+      setCategory(text);
+    }
+  };
+  const handleSubmit = () => {};
+  return (
+    <form>
+      <input
+        onChange={(e) => handleChange(e.target)}
+        value={title}
+        type="text"
+      />
+      <select onChange={(e) => handleChange(e.target)}>
+        {CATEGORIES.map((category) => (
+          <option key={`${category}`}>{category}</option>
+        ))}
+      </select>
+      <input type="submit" />
+    </form>
+  );
+};
 
 export default BooksForm;
