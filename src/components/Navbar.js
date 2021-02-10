6265f6ebee94609edf4478d5cdf91321d09639d2
khalilhamdii <khaalil.hamdi@gmail.com>
2021-02-10 17:28:40 +0100

    Add styling to body and minor changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,33 +7,48 @@ const StyledNav = styled.nav`
 `;
 
 const StyledLi = styled.li`
-font-family: Montserrat, sans-serif;
-font-size: 13px;
-letter-spacing: 1.9px;
-color: #121212;
+  font-family: Montserrat, sans-serif;
+  font-size: 13px;
+  letter-spacing: 1.9px;
+  color: #121212;
 `;
 
 const StyledLink = styled.a`
-font-size: 30px;
-color: #0290ff !important;
-font-family: Montserrat, sans-serif;
-font-style: normal;
-font-weight: bold;
-line-height: 37px;
+  font-size: 30px;
+  color: #0290ff !important;
+  font-family: Montserrat, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  line-height: 37px;
 `;
 
 const Navbar = () => (
-  <StyledNav className="navbar navbar-light navbar-expand-md d-xl-flex navigation-clean">
+  <StyledNav className="navbar navbar-light navbar-expand-md d-xl-flex navigation-clean bg-white">
     <div className="container">
-      <StyledLink className="navbar-brand" href="/">Bookstore CMS</StyledLink>
-      <button type="button" data-toggle="collapse" className="navbar-toggler" data-target="#navcol-1">
+      <StyledLink className="navbar-brand" href="/">
+        Bookstore CMS
+      </StyledLink>
+      <button
+        type="button"
+        data-toggle="collapse"
+        className="navbar-toggler"
+        data-target="#navcol-1"
+      >
         <span className="sr-only">Toggle navigation</span>
         <span className="navbar-toggler-icon" />
       </button>
       <div className="collapse navbar-collapse" id="navcol-1">
         <ul className="nav navbar-nav mr-auto">
-          <StyledLi className="nav-item"><a className="nav-link" href="/">BOOKS</a></StyledLi>
-          <StyledLi className="nav-item"><a className="nav-link" href="/">CATEGORIES</a></StyledLi>
+          <StyledLi className="nav-item">
+            <a className="nav-link" href="/">
+              BOOKS
+            </a>
+          </StyledLi>
+          <StyledLi className="nav-item">
+            <a className="nav-link" href="/">
+              CATEGORIES
+            </a>
+          </StyledLi>
         </ul>
       </div>
       <i className="fa fa-user-circle-o fa-2x" style={{ color: '#0290ff' }} />
